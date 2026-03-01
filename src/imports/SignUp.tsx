702a1649631dfdd07d72d3b4cc807ba8641f5785
kanikani27 <crazyyellow05@gmail.com>
2026-03-01 17:07:28 +0530

    Base components uploaded

---
+++ src/imports/SignUp.tsx
@@ -0,0 +1,286 @@
+import svgPaths from "./svg-b3x87pu8sf";
+import imgChrisLee70L1TDai6RMUnsplash1 from "figma:asset/c230ffba520fab60716f712257d7f6a7fc48a42f.png";
+
+function Frame() {
+  return (
+    <div className="col-1 content-stretch flex h-[52.528px] items-start ml-0 mt-0 relative row-1">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[32px] text-black">Get Started Now</p>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="content-stretch flex items-start relative shrink-0">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[14px] text-black">Name</p>
+    </div>
+  );
+}
+
+function Frame7() {
+  return (
+    <div className="content-stretch flex items-start justify-center relative shrink-0">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[#d9d9d9] text-[10px]">Enter your name</p>
+    </div>
+  );
+}
+
+function Frame2() {
+  return (
+    <div className="h-[32px] relative rounded-[10px] shrink-0 w-[404px]">
+      <div className="content-stretch flex items-start overflow-clip pl-[10px] py-[10px] relative rounded-[inherit] size-full">
+        <Frame7 />
+      </div>
+      <div aria-hidden="true" className="absolute border border-[#d9d9d9] border-solid inset-0 pointer-events-none rounded-[10px]" />
+    </div>
+  );
+}
+
+function Frame3() {
+  return (
+    <div className="col-1 content-stretch flex flex-col h-[58px] items-start ml-0 mt-[110.53px] relative row-1 w-[404px]">
+      <Frame1 />
+      <Frame2 />
+    </div>
+  );
+}
+
+function Frame4() {
+  return (
+    <div className="content-stretch flex items-start relative shrink-0">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[14px] text-black">Email address</p>
+    </div>
+  );
+}
+
+function Frame8() {
+  return (
+    <div className="content-stretch flex items-start justify-center relative shrink-0">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[#d9d9d9] text-[10px]">Enter your email</p>
+    </div>
+  );
+}
+
+function Frame5() {
+  return (
+    <div className="h-[32px] relative rounded-[10px] shrink-0 w-[404px]">
+      <div className="content-stretch flex items-center overflow-clip pl-[10px] py-[10px] relative rounded-[inherit] size-full">
+        <Frame8 />
+      </div>
+      <div aria-hidden="true" className="absolute border border-[#d9d9d9] border-solid inset-0 pointer-events-none rounded-[10px]" />
+    </div>
+  );
+}
+
+function Frame6() {
+  return (
+    <div className="content-stretch flex items-start relative shrink-0">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[14px] text-black">Password</p>
+    </div>
+  );
+}
+
+function Frame10() {
+  return (
+    <div className="content-stretch flex items-start justify-center relative shrink-0">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[#d9d9d9] text-[10px]">Name</p>
+    </div>
+  );
+}
+
+function Frame9() {
+  return (
+    <div className="h-[32px] relative rounded-[10px] shrink-0 w-[404px]">
+      <div className="content-stretch flex items-center overflow-clip pl-[10px] py-[10px] relative rounded-[inherit] size-full">
+        <Frame10 />
+      </div>
+      <div aria-hidden="true" className="absolute border border-[#d9d9d9] border-solid inset-0 pointer-events-none rounded-[10px]" />
+    </div>
+  );
+}
+
+function Frame12() {
+  return (
+    <div className="absolute bg-white content-stretch flex items-center justify-center left-[190px] overflow-clip px-[3px] top-0 w-[20px]">
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[9px] text-black">Or</p>
+    </div>
+  );
+}
+
+function Frame11() {
+  return (
+    <div className="col-1 h-[15.321px] ml-[3px] mt-[482.6px] relative row-1 w-[400px]">
+      <div className="absolute h-0 left-0 top-[9px] w-[400px]">
+        <div className="absolute inset-[-2px_0_0_0]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 400 2">
+            <line id="Line 2" stroke="var(--stroke-0, #F5F5F5)" strokeWidth="2" x2="400" y1="1" y2="1" />
+          </svg>
+        </div>
+      </div>
+      <Frame12 />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="col-1 grid-cols-[max-content] grid-rows-[max-content] inline-grid ml-0 mt-[343.62px] place-items-start relative row-1">
+      <p className="col-1 font-['Poppins:Medium',sans-serif] h-[15.321px] ml-[15px] mt-0 not-italic relative row-1 text-[9px] text-black w-[128px] whitespace-pre-wrap">
+        <span className="leading-[normal]">{`I agree to the `}</span>
+        <span className="[text-decoration-skip-ink:none] decoration-solid leading-[normal] underline">{`terms & policy`}</span>
+      </p>
+      <div className="border border-black border-solid col-1 h-[9.849px] ml-0 mt-[2.19px] rounded-[2px] row-1 w-[9px]" />
+    </div>
+  );
+}
+
+function Group1() {
+  return (
+    <div className="grid-cols-[max-content] grid-rows-[max-content] inline-grid leading-[0] place-items-start relative shrink-0">
+      <p className="col-1 font-['Poppins:Medium',sans-serif] ml-0 mt-0 not-italic relative row-1 text-[0px] text-[14px] text-black">
+        <span className="leading-[normal]">{`Have an account?  `}</span>
+        <span className="leading-[normal] text-[#0f3dde]">Sign In</span>
+      </p>
+    </div>
+  );
+}
+
+function Frame13() {
+  return (
+    <div className="col-1 content-stretch flex flex-col h-[22.981px] items-start ml-[113px] mt-[615.02px] relative row-1">
+      <Group1 />
+    </div>
+  );
+}
+
+function Frame15() {
+  return <div className="content-stretch flex items-start justify-center shrink-0" />;
+}
+
+function Frame14() {
+  return (
+    <div className="bg-[#3a5b22] h-[32px] relative rounded-[10px] shrink-0 w-[404px]">
+      <div className="content-stretch flex items-center overflow-clip pl-[10px] py-[10px] relative rounded-[inherit] size-full">
+        <Frame15 />
+      </div>
+      <div aria-hidden="true" className="absolute border border-[#3a5b22] border-solid inset-0 pointer-events-none rounded-[10px]" />
+    </div>
+  );
+}
+
+function Icons8Google() {
+  return (
+    <div className="relative shrink-0 size-[24px]" data-name="icons8-google 1">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 24 24">
+        <g id="icons8-google 1">
+          <path d={svgPaths.p257b0b00} fill="var(--fill-0, #FFC107)" id="Vector" />
+          <path d={svgPaths.p3347ed00} fill="var(--fill-0, #FF3D00)" id="Vector_2" />
+          <path d={svgPaths.p19b71500} fill="var(--fill-0, #4CAF50)" id="Vector_3" />
+          <path d={svgPaths.p3dad0040} fill="var(--fill-0, #1976D2)" id="Vector_4" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Frame17() {
+  return (
+    <div className="content-stretch flex gap-[10px] items-center relative shrink-0">
+      <Icons8Google />
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[12px] text-black">Sign in with Google</p>
+    </div>
+  );
+}
+
+function Frame16() {
+  return (
+    <div className="col-1 ml-0 mt-[560px] relative rounded-[10px] row-1">
+      <div className="content-stretch flex flex-col items-start overflow-clip px-[20px] py-[4px] relative rounded-[inherit]">
+        <Frame17 />
+      </div>
+      <div aria-hidden="true" className="absolute border border-[#d9d9d9] border-solid inset-0 pointer-events-none rounded-[10px]" />
+    </div>
+  );
+}
+
+function Icons8AppleLogo() {
+  return (
+    <div className="relative shrink-0 size-[24px]" data-name="icons8-apple-logo 1">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 24 24">
+        <g clipPath="url(#clip0_1_175)" id="icons8-apple-logo 1">
+          <g id="Vector">
+            <path d={svgPaths.p91f2700} fill="var(--fill-0, black)" />
+            <path d={svgPaths.p129e6660} fill="var(--fill-0, black)" />
+            <path d={svgPaths.p3b588600} fill="var(--fill-0, black)" />
+          </g>
+        </g>
+        <defs>
+          <clipPath id="clip0_1_175">
+            <rect fill="white" height="24" width="24" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function Frame19() {
+  return (
+    <div className="content-stretch flex gap-[10px] items-center justify-center relative shrink-0">
+      <Icons8AppleLogo />
+      <p className="font-['Poppins:Medium',sans-serif] leading-[normal] not-italic relative shrink-0 text-[12px] text-black">Sign in with Apple</p>
+    </div>
+  );
+}
+
+function Frame18() {
+  return (
+    <div className="col-1 ml-[213px] mt-[558px] relative rounded-[10px] row-1 w-[190px]">
+      <div className="content-stretch flex flex-col items-center justify-center overflow-clip px-[20px] py-[4px] relative rounded-[inherit] w-full">
+        <Frame19 />
+      </div>
+      <div aria-hidden="true" className="absolute border border-[#d9d9d9] border-solid inset-0 pointer-events-none rounded-[10px]" />
+    </div>
+  );
+}
+
+function Group2() {
+  return (
+    <div className="grid-cols-[max-content] grid-rows-[max-content] inline-grid leading-[0] place-items-start relative shrink-0">
+      <Frame />
+      <Frame3 />
+      <div className="col-1 content-stretch flex flex-col h-[58px] items-start ml-0 mt-[188.23px] relative row-1 w-[404px]">
+        <Frame4 />
+        <Frame5 />
+      </div>
+      <div className="col-1 content-stretch flex flex-col h-[58px] items-start ml-0 mt-[265.92px] relative row-1 w-[404px]">
+        <Frame6 />
+        <Frame9 />
+      </div>
+      <Frame11 />
+      <Group />
+      <Frame13 />
+      <div className="bg-[#edf2f7] col-1 h-[35.019px] ml-0 mt-[388.49px] relative row-1 w-[404px]">
+        <div className="absolute content-stretch flex flex-col items-start left-0 top-0 w-[404px]">
+          <Frame14 />
+        </div>
+        <p className="absolute font-['Poppins:Bold',sans-serif] leading-[normal] left-[185px] not-italic text-[13px] text-white top-[6px]">Signup</p>
+      </div>
+      <Frame16 />
+      <Frame18 />
+    </div>
+  );
+}
+
+export default function SignUp() {
+  return (
+    <div className="bg-white relative size-full" data-name="Sign up">
+      <div className="absolute content-stretch flex h-[638px] items-start left-[calc(10%+21.4px)] pb-[64px] top-[202px]">
+        <Group2 />
+      </div>
+      <div className="absolute h-[1042px] left-[calc(40%+139.6px)] rounded-bl-[45px] rounded-tl-[45px] top-0 w-[781.5px]" data-name="chris-lee-70l1tDAI6rM-unsplash 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-bl-[45px] rounded-tl-[45px] size-full" src={imgChrisLee70L1TDai6RMUnsplash1} />
+      </div>
+    </div>
+  );
+}
